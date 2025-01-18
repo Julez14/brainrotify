007d82a20ff993d09c2d2eb78772ee6abd7f5b89
jocelyn-chang <jocelyn00jc@gmail.com>
2025-01-17 21:41:31 -0500

    set up convex

---
+++ brainrot/app/index.tsx
@@ -0,0 +1,34 @@
+// import { Text, View } from "react-native";
+
+// export default function Index() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Edit app/index.tsx to edit this screen.</Text>
+//     </View>
+//   );
+// }
+
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  const tasks = useQuery(api.tasks.get);
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {tasks?.map(({ _id, text }) => <Text key={_id}>{text}</Text>)}
+    </View>
+  );
+}
